648143e6a18a260383d614b8f70b3ad68e1be714
Antônio Altvater <46973583+antonioaltvater@users.noreply.github.com>
2024-11-03 14:04:51 -0300

    feat(update-filtros)

--- src/Pages/Programacao.tsx
+++ src/Pages/Programacao.tsx
@@ -1,77 +1,87 @@
-// Programacao.tsx
-import { FormControl, InputLabel, makeStyles, MenuItem, Select, Typography } from '@material-ui/core';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import { Chip } from '@mui/material';
-import { addDays, isBefore, isSameDay } from 'date-fns';
 import React, { useCallback, useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import {
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+  Chip
+} from '@mui/material';
+import { useParams, useNavigate } from 'react-router-dom';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { makeStyles } from '@mui/styles';
 import EventoCard from '../Cards/EventoCard';
-import { EventoInterface, EVENTOS_CONSTANTS } from '../constants'; // Importando eventos diretamente
 import Header2 from '../Header2/Header2';
+import { addDays, isBefore, isSameDay } from 'date-fns';
+import { EventoInterface, EVENTOS_CONSTANTS } from '../constants';
 
+// Defina a interface do evento
+const eventoConstants = EVENTOS_CONSTANTS;
+
+// Função para buscar eventos (simulando chamada de API)
+const fetchEventos = async (): Promise<EventoInterface[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(eventoConstants);
+    }, 2000); // Simula atraso de 2 segundos
+  });
+};
+
+// Defina os estilos
 const useStyles = makeStyles({
   root: {
-    minHeight: '100vh',
-    background: '#d8c9af',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundAttachment: 'fixed',
-  },
-  opacity: {
-    padding: '16px',
-  },
-  eventoPage: {
     display: 'flex',
     flexDirection: 'column',
-    width: '100%'
+    alignItems: 'center',
   },
-  message: {
-    background: '#fff',
-    padding: '2rem .5rem',
-    textAlign: 'center',
-    marginTop: '20px',
-    color: '#461d1d',
+  opacity: {
+    opacity: 0.95,
+  },
+  eventoPage: {
+    maxWidth: 800,
+    width: '100%',
+    padding: '20px',
   },
   eventHeader: {
     display: 'flex',
-    alignItems: 'center',
     justifyContent: 'space-between',
-    gap: '16px',
-    width: '100%',
-    boxSizing: 'border-box',
-    padding: '.5rem',
-    flexWrap: 'wrap',
+    alignItems: 'center',
+    marginBottom: '20px',
+  },
+  message: {
+    textAlign: 'center',
+    marginTop: '20px',
   },
   verProgramacaoBtn: {
-    all: 'unset',
-    cursor: 'pointer',
-    fontFamily: 'roboto',
-    border: 'solid 1px #efefe',
-    borderRadius: '10px',
-    padding: '.2rem 1rem',
-    marginTop: '1rem',
-    backgroundColor: '#3da53d',
-    color: '#f2f5f2',
-    transition: 'background-color 0.3s ease',
-    '&:hover': {
-      backgroundColor: '#2e7c2e',
-      color: '#ffffff',
-    },
-  }
+    marginTop: '10px',
+  },
 });
 
+// Componente principal
 const Programacao: React.FC = () => {
   const classes = useStyles();
   const { filtro } = useParams<{ filtro?: string }>();
   const navigate = useNavigate();
-  const eventos = EVENTOS_CONSTANTS;
-  console.log('EVENTOS_2', filtro)
 
-  const [filteredEvents, setFilteredEvents] = useState<EventoInterface[]>(eventos);
+  // Estado para eventos e carregamento
+  const [eventos, setEventos] = useState<EventoInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [filteredEvents, setFilteredEvents] = useState<EventoInterface[]>([]);
   const [selectedFilter, setSelectedFilter] = useState<string>(filtro || '15dias');
 
-  console.log(selectedFilter, 'filtros');
+  // Função assíncrona para carregar eventos
+  const carregarEventos = async () => {
+    setLoading(true);
+    try {
+      const data = await fetchEventos(); // Substitua por chamada real à API
+      setEventos(data);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  // Função de filtragem
   const filtrarEventos = useCallback(() => {
     const hoje = new Date();
     let eventosFiltrados = [...eventos];
@@ -98,65 +108,79 @@ const Programacao: React.FC = () => {
     }
 
     eventosFiltrados.sort((a, b) => a.data.getTime() - b.data.getTime());
-    console.log(eventosFiltrados)
     setFilteredEvents(eventosFiltrados);
   }, [eventos, selectedFilter]);
 
+  // Carregar eventos ao montar o componente
   useEffect(() => {
-    filtrarEventos();
-  }, [filtrarEventos]);
+    carregarEventos();
+  }, []);
 
+  // Refilar eventos quando os eventos ou o filtro mudam
+  useEffect(() => {
+    if (!loading) {
+      filtrarEventos();
+    }
+  }, [filtrarEventos, loading]);
+
+  // Lidar com a mudança de filtro
   const handleFilterChange = (filtro: string) => {
     setSelectedFilter(filtro);
     navigate(`/programacao/${filtro}`);
   };
 
+  // Função para compartilhar no WhatsApp (exemplo simples)
   const handleShare = () => {
-    const whatsappUrl = `https://wa.me/?text=Confira as novidades da Agenda Unificada do Novembro Negro Sanca: ${encodeURIComponent(window.location.href)}`;
-    window.open(whatsappUrl, '_blank');
+    const message = 'Confira a programação de eventos!';
+    const url = `https://wa.me/?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
   };
 
+  // Renderizar componente
   return (
     <div className={classes.root}>
       <Header2 />
       <div className={classes.opacity}>
         <div className={classes.eventoPage}>
-          <div className={classes.eventHeader}>
-            <FormControl>
-              <InputLabel variant="standard">
-                Exibir
-              </InputLabel>
-              <Select variant='standard'
-                value={selectedFilter}
-                onChange={(e) => handleFilterChange(e.target.value as string)}
-                displayEmpty
-              >
-                <MenuItem value="todos">Programação Completa</MenuItem>
-                <MenuItem value="15dias">Próximos 15 Dias</MenuItem>
-                <MenuItem value="7dias">Próximos 7 Dias</MenuItem>
-                <MenuItem value="hoje">Hoje</MenuItem>
-              </Select>
-            </FormControl>
-
-            <Chip onClick={handleShare} color='success' label="Compartilhar no WhatsApp" icon={<WhatsAppIcon />} />
-          </div>
-
-          {/* Verificação de Eventos Filtrados */}
-          {filteredEvents.length === 0 ? (
-            <div className={classes.message}>
-              <Typography variant="h6">Não foi encontrado nenhum evento para a pesquisa selecionada.</Typography>
-              <button className={classes.verProgramacaoBtn} onClick={() => handleFilterChange('todos')} >
-                Ver Programação Completa
-              </button>
-            </div>
+          {/* Exibe carregamento enquanto os eventos estão sendo carregados */}
+          {loading ? (
+            <CircularProgress />
           ) : (
-            // Lista de Eventos
-            filteredEvents.map((evento) => (
-              <EventoCard key={evento.key.toString()} evento={evento} />
-            ))
+            <>
+              <div className={classes.eventHeader}>
+                <FormControl>
+                  <InputLabel variant="standard">Exibir</InputLabel>
+                  <Select
+                    variant='standard'
+                    value={selectedFilter}
+                    onChange={(e) => handleFilterChange(e.target.value as string)}
+                    displayEmpty
+                  >
+                    <MenuItem value="todos">Programação Completa</MenuItem>
+                    <MenuItem value="15dias">Próximos 15 Dias</MenuItem>
+                    <MenuItem value="7dias">Próximos 7 Dias</MenuItem>
+                    <MenuItem value="hoje">Hoje</MenuItem>
+                  </Select>
+                </FormControl>
+                <Chip onClick={handleShare} color='success' label="Compartilhar no WhatsApp" icon={<WhatsAppIcon />} />
+              </div>
+
+              {/* Renderiza a lista de eventos ou mensagem se vazia */}
+              {filteredEvents.length === 0 ? (
+                <div className={classes.message}>
+                  <Typography variant="h6">Não foi encontrado nenhum evento para a pesquisa selecionada.</Typography>
+                  <button className={classes.verProgramacaoBtn} onClick={() => handleFilterChange('todos')} >
+                    Ver Programação Completa
+                  </button>
+                </div>
+              ) : (
+                filteredEvents.map((evento) => (
+                  <EventoCard key={evento.key.toString()} evento={evento} />
+                ))
+              )}
+            </>
           )}
         </div>
-
       </div>
     </div>
   );
