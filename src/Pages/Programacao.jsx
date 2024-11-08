4b742d81db89778a96e71190c011c82ac4548304
Antônio Altvater <46973583+antonioaltvater@users.noreply.github.com>
2024-11-08 01:21:44 -0300

    update

--- src/Pages/Programacao.jsx
+++ src/Pages/Programacao.jsx
@@ -1,17 +1,43 @@
 // eslint-disable-next-line no-unused-vars
+import { Button, ButtonGroup } from '@material-ui/core';
 import styles from './Programacao.css';
 
 
 
 export default function Programacao() {
-
-
-
-
-
   return (
     <div className="root">
       <div className='evento-list'>
+        <div className='main-card'>
+          <span className='agenda-titulo'>Agenda Unificada <br></br> Novembro Negro</span>
+          <span className='agenda-subtile'>São Carlos - 2024</span>
+          <span className='agenda-unificada-description'>A “Agenda Unificada Novembro Negro - São Carlos" é resultante de um mapeamento de atividades e ações que serão realizadas em novembro e estão voltadas para promoção da luta antirracista e para valorização da comunidade negra na nossa cidade. É uma iniciativa conjunta e colaborativa de diversas instituições e da sociedade civil de São Carlos. Compartilharemos semanalmente os diversos eventos que integram a comemoração do mês da consciência negra. Acompanhe, compartilhe e envie sugestões!<br></br>
+          </span>
+          <span className='agenda-unificada-description' >  Para inscrever seu evento preencha o formulário, entre em contato pelo Instagram @novembronegrosanca ou nos envie um email.
+            Vamos nos aquilombar São Carlos!
+            Todos os eventos desta semana são gratuitos!</span>
+
+
+          <ButtonGroup className='button-grp' variant="contained" aria-label="Basic button group">
+            <Button
+              onClick={() => window.open('https://forms.gle/2MFFpzD7SX2Vqh9S9', '_blank')}
+              color='primary'
+            >
+              Formulário
+            </Button>
+
+            <Button
+              color='secondary'
+              onClick={() => window.location.href = 'mailto:novembronegrosaocarlos@gmail.com'}
+            >
+              E-mail
+            </Button>
+          </ButtonGroup>
+
+        </div>
+        <div className='separator'>
+          <span className='separator-title'>De 7 - 10 de Novembro</span>
+        </div>
         <div className='evento-card green-card'>
           <h3 className='evento-titulo'>3ª Edição do Prêmio Chica Lopes</h3>
           <span className='evento-data'>07/11 - 19h</span>
@@ -33,14 +59,18 @@ export default function Programacao() {
           <span className='evento-local'>Local: SESC São Carlos.</span>
           <span className='evento-descricao'>
             O grupo homenageia Antônio Candeia Filho, ou simplesmente Candeia, cantor e compositor que neste ano completaria 89 anos de vida, uma grande referência na história da música brasileira.
+            Contato: (16) 99991-8193
           </span>
         </div>
         <div className='evento-card black-card'>
           <h3 className='evento-titulo red-title'>Batalha da Alcatéia</h3>
           <span className='evento-data'>10/11 - 16h - 22h</span>
           <span className='evento-local'>Local: Estação Ferroviária de São Carlos</span>
+          <span className='evento-descricao'>
+            Festival de Rap para valorizar a cultura e artistas locais.
+            Contato: 16991172038
+          </span>
         </div>
-
       </div>
 
 
